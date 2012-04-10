ffaedefa31dce8afb040375205eb205e891f2c56
Olivier Sallou <olivier.sallou@debian.org>
2012-04-10 11:40:59 +0200

    remove not necessary methods

--- scripts/usr/share/xgrid/web/xgridnode.rb
+++ scripts/usr/share/xgrid/web/xgridnode.rb
@@ -13,18 +13,6 @@ class XgridNode
   property :status, Integer
   property :info, String
 
-  def add_to_sge()
-
-  end
-
-  def self.del_from_sge()
-
-  end
-
-  def self.get_sge_stats()
-
-  end
-
 end
 
 class XgridEC2
@@ -32,14 +20,6 @@ class XgridEC2
 
    property :ec2key, String, :key => true
    property :ec2pwd, String
-
-   def self.add()
-
-   end
-
-   def self.remove()
-
-   end
 
 end
 
