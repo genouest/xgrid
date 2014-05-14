0dc0e4361ab81dc92697b6cee93a35d3136debc9
Olivier Sallou <olivier.sallou@irisa.fr>
2014-05-14 13:05:44 +0200

    delay removal of firstboot file

--- usr/share/xgrid/init.sh
+++ usr/share/xgrid/init.sh
@@ -31,7 +31,6 @@ if [ -z $XGRID_EC2_PORT ]; then
 fi
 
 if [ -e /var/lib/xgrid/firstboot ]; then
-  rm /var/lib/xgrid/firstboot
 
   apt-get update
 
@@ -92,6 +91,10 @@ do
   echo "Execute plugin init: "$f
   bash $f  >> /var/log/xgrid.log
 done
+
+if [ -e /var/lib/xgrid/firstboot ]; then
+  rm /var/lib/xgrid/firstboot
+fi
 
 if [ -z $XGRIDMASTER ]; then
   exportfs -ra
