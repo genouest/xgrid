58d90e4fd17d80c9bc94af72bddd76c16d21c8a0
Cyril Monjeaud <Cyril.Monjeaud@irisa.fr>
2015-01-30 17:27:41 +0100

    change CHEF key functionality

--- usr/share/xgrid/plugins/cookbook/init.sh
+++ usr/share/xgrid/plugins/cookbook/init.sh
@@ -20,16 +20,21 @@ gem install chef -v 11.16.4 --no-ri --no-rdoc
 
 if [ -n "$CHEFSERVER" ]; then
 
-  # chef section configuration
   sed -i "s/@@chefserver = '.*'/@@chefserver = '"$CHEFSERVER"'/" /usr/share/xgrid/web/xgridconfig.rb
-  echo "-----BEGIN RSA PRIVATE KEY-----" > /usr/share/xgrid/web/chef_keys/chef-validator.pem
-  echo $CHEFVALIDATIONKEY | sed s/" "/"\n"/g >> /usr/share/xgrid/web/chef_keys/chef-validator.pem
-  echo "-----END RSA PRIVATE KEY-----" >> /usr/share/xgrid/web/chef_keys/chef-validator.pem
+
+  # if custom key is submit
+  if [ $CHEFVALIDATIONKEY != "default" ]; then
+
+	  # chef custom key, erase default key
+	  echo "-----BEGIN RSA PRIVATE KEY-----" > /usr/share/xgrid/web/chef_keys/chef-validator.pem
+	  echo $CHEFVALIDATIONKEY | sed s/" "/"\n"/g >> /usr/share/xgrid/web/chef_keys/chef-validator.pem
+	  echo "-----END RSA PRIVATE KEY-----" >> /usr/share/xgrid/web/chef_keys/chef-validator.pem
+  fi
 
   # add module in xgrid.yaml
   sed -i "s/modules: Xgrid/modules: Xgrid,XgridCookbook/" /etc/xgrid/xgrid.yaml
 
   # edit the welcome apache page
-  sed -i "s/<\/body>/<p>You can install some CHEF cookbooks via <a href=\/xgrid>Xgrid<\/a> web application<\/p><\/body>/" /var/www/index.html
+  sed -i "s/<\/div><div class=\"footer\">/<h2>CHEF cookbooks installation<\/h2><p>You can install some CHEF <b>cookbooks<\/b> via <a href=\/xgrid>Xgrid<\/a> web application<\/p><\/div><div class=\"footer\">/" /var/www/index.html
 
 fi
